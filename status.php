6fecc2cda1680807d620e13c8a0ec626bd83a9f3
iwebpohwu <121675237+iwebpohwu@users.noreply.github.com>
2025-03-11 00:11:57 +0800

    status.php

--- status.php
+++ status.php
@@ -11,9 +11,9 @@
   require_once('ip.php');
   $time = time();
   $conn = mysqli_connect("localhost","qd","qdqdqd","qd");
-  //数据库
+  //資料庫
 
-  echo"<h3>未签到：</h3>";
+  echo"<h3>未簽到：</h3>";
   echo "<table rules=none>";
   $j = 0;
   $sql = "SELECT * FROM `user`";
@@ -37,7 +37,7 @@
 
 
 
-  echo "<br /><br /><br /><h3>已签到:</h3>";
+  echo "<br /><br /><br /><h3>已簽到:</h3>";
   echo "<table rules=none>";
   $i = 0;
   $sql = "SELECT * FROM `user` order by time";
@@ -58,8 +58,8 @@
     }
   }
   echo "</table>";
-  echo "<br /><br /><br /><h3>总计：</h3>签到" . $i . "人,未签到 " . $j . "人";
-  echo "<br /><br /><a href=\"./forsignin.php\">历史记录</a><br />";
-  echo "Tip:清除记录功能暂时移除,由程序于每天在签到前1小时左右自动清除";
+  echo "<br /><br /><br /><h3>總計：</h3>簽到" . $i . "人,未簽到 " . $j . "人";
+  echo "<br /><br /><a href=\"./forsignin.php\">歷史記錄</a><br />";
+  echo "Tip:清除記錄功能暫時移除,由程式於每天在簽到前1小時左右自動清除";
   ?>
 </body>
