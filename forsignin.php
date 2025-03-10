f02012a388ee57d4cfb64bcc7b8b924e5088ffb2
iwebpohwu <121675237+iwebpohwu@users.noreply.github.com>
2025-03-11 00:08:53 +0800

    forsignin.php

--- forsignin.php
+++ forsignin.php
@@ -13,34 +13,34 @@ $c = $_GET['c'];
 $s = $_GET['s'];
 if(!empty($s) && !empty($c))
 {
-  echo "<h2>$c$s 统计记录</h2>";
+  echo "<h2>$c$s 統計記錄</h2>";
   $sql = "SELECT * FROM `history` WHERE `timec`= '$c' AND `times`='$s';";
   $re = mysqli_query($conn,$sql);
   $arr = mysqli_fetch_assoc($re);
-  echo "<h3>签到：</h3>" . $arr['y'] . "人";
-  echo "<h3>未签到：</h3>" . $arr['n'] . "人";
-  echo "<h3>未签到名单：</h3>";
+  echo "<h3>簽到：</h3>" . $arr['y'] . "人";
+  echo "<h3>未簽到：</h3>" . $arr['n'] . "人";
+  echo "<h3>未簽到名單：</h3>";
   if(!empty($arr['name']))
   {
     echo $arr['name'];
   }else{
-    echo "无";
+    echo "無";
   }
   echo "<br /><br /><br /><center><a href=\"./forsignin.php\">返回</a></center>";
 exit();
 }
 $sql = "SELECT * FROM `history` ORDER BY time DESC;";
 $re = mysqli_query($conn,$sql);
-echo"<h3>所有记录：</h3>";
+echo"<h3>所有記錄：</h3>";
 echo "<table rules=none>
 <tr>
-<td>时间</td>
+<td>時間</td>
 <td></td>
-<td>签到</td>
+<td>簽到</td>
 <td></td>
-<td>未签到</td>
+<td>未簽到</td>
 <td></td>
-<td>未签到名单</td>
+<td>未簽到名單</td>
 ";
 while ($row = mysqli_fetch_object($re)) 
 {
